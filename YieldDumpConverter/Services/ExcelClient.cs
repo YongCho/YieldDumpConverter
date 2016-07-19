cbc68bc043ed8f590fb539a623b83806248c43a5
YongCho <yongjec@gmail.com>
2016-07-18 21:40:21 -0400

    Move Excel functionality out of MainWindow.

---
+++ YieldDumpConverter/Services/ExcelClient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace YieldDumpConverter.Services
+{
+    class ExcelClient
+    {
+        /// <summary>
+        /// Opens a new Excel window and pastes the text into the default worksheet.
+        /// </summary>
+        /// <param name="textToPaste">Text to be pasted into the Excel</param>
+        public static void OpenInExcel(string textToPaste)
+        {
+            try
+            {
+                OpenInExcelInternal(textToPaste);
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+        private static void OpenInExcelInternal(string textToPaste)
+        {
+            Excel.Application xlApp = null;
+            Excel.Workbook xlWorkbook = null;
+
+            try
+            {
+                xlApp = new Excel.Application();
+                if (xlApp == null)
+                {
+                    MessageBox.Show("Unable to open Excel.");
+                    return;
+                }
+
+                // Create a new empty workbook.
+                xlWorkbook = xlApp.Workbooks.Add();
+
+                // Copy and paste the textbox content to the active worksheet.
+                Clipboard.Clear();
+                Clipboard.SetText(textToPaste);
+                xlWorkbook.ActiveSheet.Paste();
+
+                // Make the Excel window visible to the user.
+                xlApp.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                // Release hidden references.
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                if (xlWorkbook != null)
+                {
+                    xlWorkbook.Close(false, Type.Missing, Type.Missing);
+                }
+
+                if (xlApp != null)
+                {
+                    int hwnd = xlApp.Application.Hwnd;
+                    xlApp.Quit();
+
+                    // In debug mode, the EXCEL.EXE process may still be alive. Kill it by the window handle.
+                    Utility.TryKillProcessByMainWindowHwnd(hwnd);
+                }
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // Release all handles now so that we don't leave dangling EXCEL.EXE processes
+                // after the user has closed the Excel window.
+                Marshal.FinalReleaseComObject(xlWorkbook);
+                Marshal.FinalReleaseComObject(xlApp);
+            }
+        }
+
+    }
+}
--- YieldDumpConverter/Views/MainWindow.xaml.cs
+++ YieldDumpConverter/Views/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
 using YieldDumpConverter.Services;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace YieldDumpConverter.Views
 {
@@ -33,66 +31,6 @@ namespace YieldDumpConverter.Views
             e.DataObject = d;
         }
 
-        /// <summary>
-        /// Opens a new Excel window and pastes the text into the worksheet.
-        /// </summary>
-        /// <param name="textToPaste">Text to be pasted into the Excel</param>
-        private void OpenInExcel(string textToPaste)
-        {
-            Excel.Application xlApp = null;
-            Excel.Workbook xlWorkbook = null;
-
-            try
-            {
-                xlApp = new Excel.Application();
-                if (xlApp == null)
-                {
-                    MessageBox.Show("Unable to open Excel.");
-                    return;
-                }
-
-                // Create a new empty workbook.
-                xlWorkbook = xlApp.Workbooks.Add();
-
-                // Copy and paste the textbox content to the active worksheet.
-                Clipboard.Clear();
-                Clipboard.SetText(textBoxMain.Text);
-                xlWorkbook.ActiveSheet.Paste();
-
-                // Make the Excel window visible to the user.
-                xlApp.Visible = true;
-            }
-            catch (Exception ex)
-            {
-                // Release hidden references.
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-
-                if (xlWorkbook != null)
-                {
-                    xlWorkbook.Close(false, Type.Missing, Type.Missing);
-                }
-
-                if (xlApp != null)
-                {
-                    int hwnd = xlApp.Application.Hwnd;
-                    xlApp.Quit();
-
-                    // In debug mode, the EXCEL.EXE process may still be alive. Kill it by the window handle.
-                    Utility.TryKillProcessByMainWindowHwnd(hwnd);
-                }
-
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                // Release all handles now so that we don't leave dangling EXCEL.EXE processes
-                // after the user has closed the Excel window.
-                Marshal.FinalReleaseComObject(xlWorkbook);
-                Marshal.FinalReleaseComObject(xlApp);
-            }
-        }
-
         private void btnCrash_Click(object sender, RoutedEventArgs e)
         {
             throw new Exception();
@@ -105,15 +43,7 @@ namespace YieldDumpConverter.Views
 
         private void OpenInExcelCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            try
-            {
-                OpenInExcel(textBoxMain.Text);
-            }
-            finally
-            {
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-            }
+            ExcelClient.OpenInExcel(textBoxMain.Text);
         }
     }
 }
